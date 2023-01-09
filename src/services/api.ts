a14d6cfcb5e123a7107d24cc4423febbadd4b079
Silvio Félix <silvio.felix32@hotmail.com>
2023-01-09 12:24:22 -0300

    add a path for CORS

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://vercel.com/silviofelix32/sf-tech-back/v1",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  },
 });
 
 export default api;
