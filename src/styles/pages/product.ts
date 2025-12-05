9f3764d7a5270b1c4fb99982137a12d47e3f0c40
SilvioFelix <silviofelix.sysmap@natura.net>
2025-12-05 15:55:13 -0300

    refactor: update product container width for improved responsiveness and layout consistency

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1200px;
+  width: 75%;
   margin: 0 auto;
   padding: 2rem 1rem;
   background-color: ${({ theme }) => theme.colors.background};
